8f055e433d2910791ed128c0a7599c1287c9bc89
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-02 15:48:14 +0200

    feat: ex8 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,4 @@
-const removeFromArray = function(array) {
-  for (let i = 1; i < arguments.length; i++) {
-    while ((index = array.indexOf(arguments[i])) > -1)
-      array.splice(index,1);
-  }
-  return array;
-};
-
+const removeFromArray = (array, ...args) => array.filter(val => !args.includes(val))
 console.log(removeFromArray([1,2,3,4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
