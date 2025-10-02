d075a5b526820e5ad98367769d8cea02c8446902
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-02 16:31:54 +0200

    chore: ex10 done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0))
     return true;
   return false;
 };
