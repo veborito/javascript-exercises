387c2f770b0386be661894517b920100dcb556c6
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-27 16:23:33 +0100

    feat: calculator finished

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  range = [...Array(number).keys()].map((number) => number + 1);
+	return range.reduce((total, curr) => total * curr, 1)
 };
 
+console.log(factorial(1))
 // Do not edit below this line
 module.exports = {
   add,
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const arr = string.split("");
+  const len = arr.length;
 
+  for (let i = 0; i < len; i++) {
+    if (arr[i] !== arr[len - 1 - i])
+      return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
