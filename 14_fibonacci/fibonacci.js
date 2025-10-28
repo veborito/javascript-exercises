450085df8c309a296be0f6e4b040d6de94775b15
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-28 11:27:45 +0100

    feat: fibonacci  finished

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number < 0)
+    return "OOPS";
+  else if (number == 0)
+    return 0;
+  else if (number == 1 || number == 2)
+    return 1;
+  return fibonacci(number - 1) + fibonacci(number - 2);
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
