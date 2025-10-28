9d62bd99525a572adffd46d317836e04489834f9
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-10-28 11:19:21 +0100

    feat: palindromes  finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,13 +1,14 @@
 const palindromes = function (string) {
-  const arr = string.split("");
+  const arr = string.replace(/[^\w]/g, "").toLowerCase().split("");
   const len = arr.length;
 
   for (let i = 0; i < len; i++) {
-    if (arr[i] !== arr[len - 1 - i])
+    if (arr[i] != arr[len - 1 - i])
       return false;
   }
   return true;
 };
 
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
